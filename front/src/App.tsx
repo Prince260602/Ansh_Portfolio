45e7cc1b7b9a4cb8270af2060ea1c0ac3606e786
Prince <princemultaniya89@gmail.com>
2025-03-30 19:38:46 +0530

    Update App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,5 @@
-import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar";
@@ -11,7 +12,7 @@ import BlogDetail from "./pages/BlogDetail";
 import ProjectDetail from "./pages/ProjectDetail";
 import AdminLogin from "./pages/Admin/Login";
 import AdminDashboard from "./pages/Admin/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
+// import ProtectedRoute from "./components/ProtectedRoute";
 
 const AppContent: React.FC = () => {
   const location = useLocation();
@@ -31,9 +32,8 @@ const AppContent: React.FC = () => {
 
         {/* Admin Routes */}
         <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<ProtectedRoute />}>
-          <Route index element={<AdminDashboard />} />
-        </Route>
+        <Route path="/admin/dashboard" element={<AdminDashboard/>}/>
+  
       </Routes>
       <Toaster position="top-right" />
     </div>
@@ -51,58 +51,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-
-// import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-// import { ThemeProvider } from "./context/ThemeContext";
-// import { Toaster } from "react-hot-toast";
-// import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import Portfolio from "./pages/Portfolio";
-// import Contact from "./pages/Contact";
-// import Blogs from "./pages/Blogs";
-// import Tech from "./pages/Tech";
-// import BlogDetail from "./pages/BlogDetail";
-// import ProjectDetail from "./pages/ProjectDetail";
-// import AdminLogin from "./pages/Admin/Login";
-// import AdminDashboard from "./pages/Admin/Dashboard";
-// import ProtectedRoute from "./components/ProtectedRoute";
-
-// const AppContent: React.FC = () => {
-//   const location = useLocation();
-//   const isAdminRoute = location.pathname.startsWith("/admin");
-
-//   return (
-//     <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
-//       {!isAdminRoute && <Navbar />}
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/portfolio" element={<Portfolio />} />
-//         <Route path="/portfolio/:slug" element={<ProjectDetail />} />
-//         <Route path="/contact" element={<Contact />} />
-//         <Route path="/blogs" element={<Blogs />} />
-//         <Route path="/blogs/:slug" element={<BlogDetail />} />
-//         <Route path="/tech" element={<Tech />} />
-
-//         {/* Admin Routes */}
-//         <Route path="/admin" element={<AdminLogin />} />
-//         <Route path="/admin/dashboard" element={<ProtectedRoute />}>
-//           <Route index element={<AdminDashboard />} />
-//         </Route>
-//       </Routes>
-//       <Toaster position="top-right" />
-//     </div>
-//   );
-// };
-
-// const App: React.FC = () => {
-//   return (
-//     <ThemeProvider>
-//       <Router>
-//         <AppContent />
-//       </Router>
-//     </ThemeProvider>
-//   );
-// };
-
-// export default App;
